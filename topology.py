9f553402a8b356489fc48cd5fc171897e9c830f7
Saibik Dey <saibikdey2@gmail.com>
2026-05-18 14:49:40 +0530

    Topology updated
    Added cycle detection and topological sorting to ServiceGraph.

--- topology.py
+++ topology.py
@@ -1,14 +1,21 @@
 from collections import defaultdict
 
+
 class ServiceGraph:
+
     def __init__(self):
+
         self.graph = defaultdict(list)
+
         self.services = set()
 
 
     def load_services(self, filename):
+
         with open(filename, 'r') as file:
+
             for line in file:
+
                 line = line.strip()
 
                 if not line:
@@ -19,41 +26,52 @@ class ServiceGraph:
                 self.graph[parent].append(child)
 
                 self.services.add(parent)
+
                 self.services.add(child)
 
 
     def dfs(self, node, visited, stack):
+
         visited.add(node)
 
         for neighbor in self.graph[node]:
+
             if neighbor not in visited:
+
                 self.dfs(neighbor, visited, stack)
 
         stack.append(node)
 
 
     def topological_sort(self):
+
         visited = set()
+
         stack = []
 
         for service in self.services:
+
             if service not in visited:
+
                 self.dfs(service, visited, stack)
 
         stack.reverse()
 
         return stack
-    
+
 
     def detect_cycle_util(self, node, visited, rec_stack, path):
 
         visited.add(node)
+
         rec_stack.add(node)
+
         path.append(node)
 
         for neighbor in self.graph[node]:
 
             if neighbor not in visited:
+
                 result = self.detect_cycle_util(
                     neighbor,
                     visited,
@@ -65,10 +83,13 @@ class ServiceGraph:
                     return result
 
             elif neighbor in rec_stack:
+
                 cycle_start = path.index(neighbor)
+
                 return path[cycle_start:] + [neighbor]
 
         rec_stack.remove(node)
+
         path.pop()
 
         return None
@@ -77,6 +98,7 @@ class ServiceGraph:
     def detect_cycle(self):
 
         visited = set()
+
         rec_stack = set()
 
         for service in self.services:
@@ -96,14 +118,41 @@ class ServiceGraph:
         return None
 
 
-g = ServiceGraph()
-g.load_services("services.txt")
 
+# -----------------------------------
+# FUNCTION FOR FLASK
+# -----------------------------------
 
-cycle = g.detect_cycle()
+def get_graph_data():
 
-if cycle:
-    print("\nCYCLE DETECTED:")
-    print(" -> ".join(cycle))
-else:
-    print("\nNo cycle found")
+    g = ServiceGraph()
+
+    g.load_services("services.txt")
+
+    cycle = g.detect_cycle()
+
+    order = []
+
+    if not cycle:
+
+        order = g.topological_sort()
+
+    edges = []
+
+    for parent in g.graph:
+
+        for child in g.graph[parent]:
+
+            edges.append([parent, child])
+
+    return {
+
+        "services": list(g.services),
+
+        "edges": edges,
+
+        "order": order,
+
+        "cycle": cycle
+
+    }
